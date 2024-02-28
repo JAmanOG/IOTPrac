48018b57fd44e5e32436d139ed309e9503174a32
Aman Jaiswal <jaman0120@gmail.com>
2024-02-28 21:27:35 +0530

    Update Light _two_Led.cpp

--- Light _two_Led.cpp
+++ Light _two_Led.cpp
@@ -1,1 +1,20 @@
+int ledLight1 = 13;
+int ledLight2 = 12;
+void setup()
+{
+ pinMode(ledLight1, OUTPUT);
+ pinMode(ledLight2, OUTPUT);
+}
+void loop()
+{
+ digitalWrite(ledLight1, HIGH);
+ digitalWrite(ledLight2, HIGH);
+ delay(1000); // Wait for 1000 millisecond(s)
+ digitalWrite(ledLight1, LOW);
+ digitalWrite(ledLight2, LOW);
+ delay(1000); // Wait for 1000 millisecond(s)
+}
 
+
+Practical Link
+https://www.tinkercad.com/things/fv9T614VlUH-light-two-led-using-breadboard?sharecode=at06qRULsRWFm6x1jcUDeSfRnNhZl2rimGTrlANHidc
