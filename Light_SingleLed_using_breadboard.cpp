d4014c87d460a658d58b827d03c675373de1ea13
Aman Jaiswal <jaman0120@gmail.com>
2024-02-28 21:46:33 +0530

    Update Light_SingleLed_using_breadboard.cpp

--- Light_SingleLed_using_breadboard.cpp
+++ Light_SingleLed_using_breadboard.cpp
@@ -1,1 +1,17 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
+
+//Practical link
+https://www.tinkercad.com/things/f8r2pd8NMMu-led-using-breadboard?sharecode=W07qfmq-CGUs-7rZSWYiVxe_0kIVPFkStuH7c74MAuA
